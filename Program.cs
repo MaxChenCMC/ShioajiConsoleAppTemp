b3905fd6cb5fa20c9f93d0133ddea6524c1e7694
Max 諶俊銘 <hhpedo@gmail.com>
2024-02-04 01:50:27 +0800

    three unchecked

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using Newtonsoft.Json;
 using Sinopac.Shioaji;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 
 
@@ -15,8 +17,18 @@ namespace Shioaji_SetQuoteCallback_PlaceOrder
             try
             {
                 SJ InitSJ = new();
-                InitSJ.Initialize();
+                InitSJ.Initialize(@"D:\Sinopac.json");
+                InitSJ.ChangePercentRankAboveMonthlyAvg(20);
+                /*=================================================
                 InitSJ.AmountRankSetQuoteCallback(15);
+
+                while (DateTime.Now < InitSJ.GetCallbackEndTime())
+                {
+                    // MxfMock(double argEntryPrice, double argStp)
+                }
+
+                InitSJ.Pnl("2024-01-10", "2024-01-10");
+                =================================================*/
                 Console.ReadLine();
             }
             catch (Exception ex)
@@ -24,22 +36,16 @@ namespace Shioaji_SetQuoteCallback_PlaceOrder
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
-
+]
 
         public class SJ
         {
-            #region seperate login method from class initialize so the login status can keeped.
+            #region login status can keeped.
             private Shioaji _api;
 
-            public void Initialize()
+            public void Initialize(string path)
             {
                 _api = new Shioaji();
-                string path = @"D:\Sinopac.json";
-                Login(path);
-            }
-
-            private void Login(string path)
-            {
                 if (!File.Exists(path))
                 {
                     throw new FileNotFoundException($"壹! File not found: {path}");
@@ -61,7 +67,7 @@ namespace Shioaji_SetQuoteCallback_PlaceOrder
             #endregion
 
 
-            #region dateformate, amt rank code to ist, subscibe,
+            #region general time setting
             private string GetValidDate()
             {
                 DateTime date = DateTime.Now.DayOfWeek switch
@@ -73,7 +79,18 @@ namespace Shioaji_SetQuoteCallback_PlaceOrder
                 return date.ToString("yyyy-MM-dd");
             }
 
+            public DateTime GetCallbackEndTime()
+            {
+                DateTime DTN = DateTime.Now;
+                bool preMktTime = DTN < DTN.Date + new TimeSpan(5, 00, 0);
+                bool onMktTime = (DTN > DTN.Date + new TimeSpan(8, 45, 0) && DTN < DTN.Date + new TimeSpan(13, 45, 0));
+                DateTime end = preMktTime ? DTN.Date + new TimeSpan(5, 00, 0) : (onMktTime ? DTN.Date + new TimeSpan(13, 45, 0) : DateTime.Now.Date + new TimeSpan(29, 0, 0));
+                return end;
+            }
+            #endregion
 
+
+            #region !! dateformate, amt rank code to ist, subscibe,
             public void AmountRankSetQuoteCallback(int TopN)
             {
                 List<string> GetAmountRankCodes = _api.Scanners(scannerType: ScannerType.AmountRank, date: GetValidDate(), count: TopN)
@@ -83,7 +100,7 @@ namespace Shioaji_SetQuoteCallback_PlaceOrder
                 {
                     try
                     {
-                        Exchange exchange = _api.Contracts.Stocks["TSE"].ContainsKey(code) ? "TSE" : "OTC";
+                        Exchange exchange = _api.Contracts.Stocks["TSE"].ContainsKey(code) ? Exchange.TSE : Exchange.OTC;
                         _api.Subscribe(_api.Contracts.Stocks[exchange.ToString()][code], QuoteType.tick, version: QuoteVersion.v1);
                     }
                     catch (Exception ex)
@@ -111,6 +128,7 @@ namespace Shioaji_SetQuoteCallback_PlaceOrder
                     tick_type = tick.tick_type,
                     close = tick.close,
                     total_amount = tick.total_amount / 100_000_000,
+                    sentiment = (tick.close - tick.low) / (tick.high - tick.low),
                 };
                 _temp.Add(rec);
 
@@ -121,9 +139,101 @@ namespace Shioaji_SetQuoteCallback_PlaceOrder
 
                 Console.WriteLine(string.Join("\n", data));
                 Console.WriteLine($"Todays focus {data.Count(x => x.tick_type == 1)} dealed at ask price");
+                Console.WriteLine($"sentiment {data.Average(x => x.sentiment * 100)}%");
                 Console.WriteLine("======================================================================================");
             }
             #endregion
+
+
+            #region !!Moving stop order
+            public void MxfMock(double argEntryPrice, double argStp)
+            {
+                while (DateTime.Now < GetCallbackEndTime())
+                {
+                    Console.WriteLine($"Would Stop at {GetCallbackEndTime().ToString()}");
+                    // place order logic here
+                    break;
+                }
+            }
+            #endregion
+
+
+            #region !!強勢股  ScannersChangePercentRank()
+            public List<dynamic> ChangePercentRankAboveMonthlyAvg(int TopN)
+            {
+                List<string> GetChangePercentRankCodes = _api.Scanners(scannerType: ScannerType.ChangePercentRank, date: GetValidDate(), count: TopN)
+                    .Where(x => x.close > 20 && x.total_amount > 500_000_000)
+                    .Select(x => (string)x.code)
+                    .ToList();
+
+                // 從openapi抓 List<string> 股票們存成 Dictionary<string, List<object>> ☛ { 股號: [股名, 月均價]}
+                var client = new HttpClient();
+                var response = client.GetAsync("https://openapi.twse.com.tw/v1/" + "exchangeReport/STOCK_DAY_AVG_ALL").Result; // 上市個股日收盤價及月平均價
+                var json = response.Content.ReadAsStringAsync().Result;
+                List<dynamic> OpenApiItems = JsonConvert.DeserializeObject<List<dynamic>>(json);
+
+                List<dynamic> _temp = new List<dynamic>();
+                foreach (var item in OpenApiItems.Where(x => GetChangePercentRankCodes.Contains(x.Code.ToString())))
+                {
+                    // openapi 目前只抓twse故不做OTC判斷
+                    var rec = new
+                    {
+                        Code = item.Code,
+                        Name = item.Name,
+                        MonthlyAveragePrice = item.MonthlyAveragePrice,
+                    };
+                    _temp.Add(rec);
+
+                }
+
+                var obj_IContracts = new List<IContract>();
+                foreach (var i in _temp.Select(x => x.Code).ToList())
+                {
+                    obj_IContracts.Add(_api.Contracts.Stocks["TSE"][i]);
+                }
+
+                List<dynamic> _temp1 = new List<dynamic>();
+                foreach (var i in _api.Snapshots(obj_IContracts))
+                {
+                    var rec = new
+                    {
+                        Code = i.code,
+                        close = i.close,
+                        change_rate = i.change_rate,
+                        total_amount = i.total_amount,
+                    };
+                    _temp1.Add(rec);
+                }
+
+
+                Console.WriteLine(string.Join("\n", _temp));
+                Console.WriteLine(_temp.Select(x => x.Code).ToList());
+                Console.WriteLine(string.Join("\n", _temp1));
+                return null;
+            }
+            #endregion
+
+
+            public void Pnl(string bgn, string end)
+            {
+                try
+                {
+                    var src = _api.ListProfitLossSummary(bgn, end, _api.FutureAccount).profitloss_summary[0];
+                    var _Pnl = new
+                    {
+                        code = (src.code.Substring(3, 5)) + (src.code[8] < 'L' ? "Call" : "Put"),
+                        direction = src.direction,
+                        entry_price = src.entry_price,
+                        cover_price = src.cover_price,
+                        netPnl = src.pnl - src.fee - src.tax
+                    };
+                    Console.WriteLine(string.Join("\n", _Pnl));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed {ex.Message}");
+                }
+            }
         }
     }
 }

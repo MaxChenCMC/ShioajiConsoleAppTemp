2e8dae8c837a156e8eff5c9f6f0f80467fc6fbe1
Max 諶俊銘 <hhpedo@gmail.com>
2024-02-03 08:40:00 +0800

    organized code.

--- Program.cs
+++ Program.cs
@@ -1,213 +1,74 @@
-using Newtonsoft.Json;
 using Sinopac.Shioaji;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text.Json;
-using System.Threading;
 
-namespace ShioajiConsoleApp
+
+namespace Shioaji_SetQuoteCallback_PlaceOrder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            SJ InitSJ = new();
-            InitSJ.Login(@"D:\Sinopac.json");
-            //==================================================================
-            //InitSJ.testCallBack();
-            //InitSJ.AmountRankSetQuoteCallback();
-
-            foreach (var i in InitSJ.ScannersChangePercentRank(50))
+            try
             {
-                i.Value.ForEach(Console.WriteLine);
-                Console.WriteLine();
+                SJ InitSJ = new();
+                InitSJ.Initialize();
+                InitSJ.AmountRankSetQuoteCallback(15);
+                Console.ReadLine();
             }
-
-
-            //==================================================================
-            Console.ReadLine();
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
 
 
         public class SJ
         {
-            #region 登入沒事不會改
-            private static Shioaji _api = new Shioaji();
+            #region seperate login method from class initialize so the login status can keeped.
+            private Shioaji _api;
 
-            public void Login(string path)
+            public void Initialize()
             {
-                string jsonString = File.ReadAllText(path);
-                JsonElement root = JsonDocument.Parse(jsonString).RootElement;
-                _api.Login(root.GetProperty("API_Key").GetString(), root.GetProperty("Secret_Key").GetString());
-            }
-            #endregion
-
-
-            #region 即時報價 testCallBack()
-            public void testCallBack()
-            {
-                _api.Subscribe(_api.Contracts.Futures["TXF"]["TXFR1"], QuoteType.tick, version: QuoteVersion.v1);
-                _api.SetQuoteCallback_v1(myQuoteCB_v1);
+                _api = new Shioaji();
+                string path = @"D:\Sinopac.json";
+                Login(path);
             }
 
-            List<dynamic> _temp = new List<dynamic>();
-            private void myQuoteCB_v1(Exchange exchange, dynamic tick)
+            private void Login(string path)
             {
-                var rec = new
+                if (!File.Exists(path))
                 {
-                    ts = DateTime.ParseExact(tick.datetime,
-                                             "yyyy/MM/dd HH:mm:ss.ffffff",
-                                             System.Globalization.CultureInfo.InvariantCulture)
-                                             .ToString("HH:mm:ss"),
-                    close = tick.close,
-                    tick_type = tick.tick_type,  // 1內、2外
-                    chg_type = tick.chg_type,    // 2漲、3平、4跌
-                    //code = tick.code, pct_chg = tick.pct_chg, underlying_price = tick.underlying_price,
-                };
-                _temp.Add(rec);
-
-                if (_temp.Last().close >= 17995)
-                {
-                    Console.WriteLine("條件達成。似乎在myQuoteCB_v1裡只要一有資料更新就會觸發一次");
+                    throw new FileNotFoundException($"File not found: {path}");
                 }
-                else
+
+                try
                 {
-                    Console.WriteLine(rec);
-                    Console.WriteLine(_temp.Max(x => x.GetType().GetProperty("close").GetValue(x)));
+                    string jsonString = File.ReadAllText(path);
+                    JsonElement root = JsonDocument.Parse(jsonString).RootElement;
+                    string apiKey = root.GetProperty("API_Key").GetString();
+                    string secretKey = root.GetProperty("Secret_Key").GetString();
+                    _api.Login(apiKey, secretKey);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Failed to login to Shioaji API.", ex);
                 }
             }
             #endregion
 
 
-            #region 模擬移停/保本/停損
-            public void MxfMock(double argEntryPrice, double argStp)
-            {
-                List<double> _temp = new List<double>();
-                while (true)
-                {
-                    double spotClose = (double)_api.Snapshots(new List<IContract>() { _api.Contracts.Futures["TXF"]["TXFR1"] })[0].close;
-                    _temp.Add(spotClose);
-                    _temp.Reverse();
-                    string result = string.Join(", ", _temp.Take(10));
-                    Console.WriteLine($"現在{spotClose}點 \t 過去 10 ticks {result}\n高點{_temp.Max()} since start");
-                    if (spotClose >= argEntryPrice + argStp)
-                    {
-                        argEntryPrice += 2;
-                        Console.WriteLine($"已拉開故到{argEntryPrice}才會被掃出場");
-                        while (true)
-                        {
-                            spotClose = (double)_api.Snapshots(new List<IContract>() { _api.Contracts.Futures["TXF"]["TXFR1"] })[0].close;
-                            _temp.Add(spotClose);
-                            if (spotClose <= (_temp.Max() - argStp))
-                            {
-                                string desc = spotClose >= argEntryPrice ? "停利" : "保本";
-                                Console.WriteLine($"★{desc}出在{spotClose}。");
-                                break;
-                            }
-                            else if (spotClose <= argEntryPrice)
-                            {
-                                Console.WriteLine($"★保本出在{spotClose}。");
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine(spotClose);
-                                Thread.Sleep(5_000);
-                            }
-                        }
-                        break;
-                    }
-                    else if (spotClose <= argEntryPrice - argStp)
-                    {
-                        Console.WriteLine($"★停損，出在{spotClose}。");
-                        break;
-                    }
-                    else Thread.Sleep(5_000);
-                }
-                Console.WriteLine("★★小台積期：" + _api.Snapshots(new List<IContract>() { _api.Contracts.Futures["QFF"]["QFFR1"] })[0].close);
-            }
-            #endregion
-
-
-            #region 強勢股  ScannersChangePercentRank()
-            public Dictionary<string, List<object>> ScannersChangePercentRank(int argCnt)
+            #region Today trade Amount Rank top N. Count how many stocks are dealed at ask price.
+            public void AmountRankSetQuoteCallback(int argCount)
             {
                 string argDate;
                 if (DateTime.Now.DayOfWeek.ToString() == "Saturday") argDate = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
                 else if (DateTime.Now.DayOfWeek.ToString() == "Sunday") argDate = DateTime.Now.AddDays(-2).ToString("yyyy-MM-dd");
                 else argDate = DateTime.Now.ToString("yyyy-MM-dd");
-
-                List<string> ls_sids = new List<string>();
-                foreach (var i in _api.Scanners(scannerType: ScannerType.ChangePercentRank, date: argDate, count: argCnt)
-                // Scanners漲幅榜用linq篩出 $20且過五億的標的 把Code存 List<string>
-                .Where(x => x.close > 20 && x.total_amount > 500_000_000))
-                {
-                    ls_sids.Add(i.code);
-                }
-
-                // 從openapi抓 List<string> 股票們存成 Dictionary<string, List<object>> ☛ { 股號: [股名, 月均價]}
-                var client = new HttpClient();
-                var response = client.GetAsync("https://openapi.twse.com.tw/v1/" + "exchangeReport/STOCK_DAY_AVG_ALL").Result; // 上市個股日收盤價及月平均價
-                var json = response.Content.ReadAsStringAsync().Result;
-                List<dynamic> src = JsonConvert.DeserializeObject<List<dynamic>>(json);
-                Dictionary<string, List<object>> retDict = new Dictionary<string, List<object>>();
-                foreach (var i in src.Where(x => ls_sids.Contains(x.Code.ToString())))
-                {
-                    List<object> _retDict = new List<object>();
-                    _retDict.Add(i.Name.ToString());
-                    _retDict.Add((Double)i.MonthlyAveragePrice);
-                    retDict.Add(i.Code.ToString(), _retDict);
-                }
-
-
-                // 同格式的Dictionary<string, List<object>> 另用Snapshots對同群標的做 { 股號: [即時價, 漲額, 成交值億]}
-                var obj_IContract = new List<IContract>();
-                foreach (var i in retDict.Keys)
-                    try { obj_IContract.Add(_api.Contracts.Stocks["TSE"][i]); }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"宣告{ex}但沒用到會報alert");
-                        obj_IContract.Add(_api.Contracts.Stocks["OTC"][i]);
-                    }
-
-                Dictionary<string, List<object>> retDict1 = new Dictionary<string, List<object>>();
-                foreach (var i in _api.Snapshots(obj_IContract))
-                {
-                    List<object> _retDict1 = new List<object>();
-                    _retDict1.Add(i.close);
-                    _retDict1.Add(i.change_rate);
-                    _retDict1.Add(Math.Round(i.total_amount / 100_000_000d, 2));
-                    retDict1.Add(i.code, _retDict1);
-                }
-
-
-                // 把兩個 Dictionary<string, List<object>>併起來，再補上乖離 ☛ { 股號:         [股名, 月均價, 即時價, 漲幅, 成交值億, 距月均價多遠]}
-                // 這個Concat的dict留到react定義 interface                  ☛  [key: string]: [string, number, number, number, number, number];
-                var ret = retDict.Concat(retDict1).GroupBy(kvp => kvp.Key).ToDictionary(g => g.Key, g => g.SelectMany(kvp => kvp.Value).ToList());
-                foreach (var i in ret.Keys)
-                {
-                    ret[i].Add(Math.Round(
-                        ((double)ret[i][2] / (double)ret[i][1]) - 1
-                        , 2));
-                }
-
-                // 篩漲超過月線x%的
-                return ret.Where(x => (Double)x.Value[5] >= 0.00).ToDictionary(x => x.Key, x => x.Value);
-            }
-            #endregion
-
-
-            #region 溫度計
-            public void AmountRankSetQuoteCallback()
-            {
-                string argDate;
-                if (DateTime.Now.DayOfWeek.ToString() == "Saturday") argDate = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
-                else if (DateTime.Now.DayOfWeek.ToString() == "Sunday") argDate = DateTime.Now.AddDays(-2).ToString("yyyy-MM-dd");
-                else argDate = DateTime.Now.ToString("yyyy-MM-dd");
-                List<string> tg = _api.Scanners(scannerType: ScannerType.AmountRank, date: argDate, count: 10).Select(x => (string)x.code).ToList();
+                List<string> tg = _api.Scanners(scannerType: ScannerType.AmountRank, date: argDate, count: argCount).Select(x => (string)x.code).ToList();
                 foreach (string i in tg)
                 {
                     try
@@ -219,11 +80,6 @@ namespace ShioajiConsoleApp
                         _api.Subscribe(_api.Contracts.Stocks["OTC"][i], QuoteType.tick, version: QuoteVersion.v1);
                     }
                 }
-                //List<string> abc = new List<string> { "TXFR1", "QFFR1" };
-                //foreach(var i in abc)
-                //{
-                //    _api.Subscribe(_api.Contracts.Futures[i.Substring(0,3)][i], QuoteType.tick, version: QuoteVersion.v1);
-                //}
                 List<dynamic> _temp = new List<dynamic>();
                 void myQuoteCB_v1(Exchange exchange, dynamic tick)
                 {
@@ -234,24 +90,19 @@ namespace ShioajiConsoleApp
                                              System.Globalization.CultureInfo.InvariantCulture)
                                              .ToString("HH:mm:ss"),
                         code = tick.code,
-                        // 1內、2外 ☛好像官方文件 tick_type 寫相反了
-                        tick_type = tick.tick_type, //== 1 ? "　↗" : "↙　" ,  
+                        tick_type = tick.tick_type,
                         close = tick.close,
                         total_amount = tick.total_amount / 100_000_000,
-                        chg_type = tick.chg_type,  // 2漲、3平、4跌 ☛ 好像只是相對昨收今平，而不是tick的即時狀況
                     };
                     _temp.Add(rec);
                     var data = _temp.GroupBy(item => item.code).Select(group => group.Last()).OrderByDescending(item => item.total_amount).ToList();
                     Console.WriteLine(string.Join("\n", data));
-                    //int test = data.Count(x => x.tick_type == 1);
-                    Console.WriteLine($"成交重心有{data.Count(x => x.tick_type == 1)}/10檔在外盤");
+                    Console.WriteLine($"Todays focus {data.Count(x => x.tick_type == 1)}/{argCount} dealed at ask price");
                     Console.WriteLine("======================================================================================");
-                    //Thread.Sleep(10_000);
                 }
                 _api.SetQuoteCallback_v1(myQuoteCB_v1);
             }
             #endregion
-
         }
     }
 }
